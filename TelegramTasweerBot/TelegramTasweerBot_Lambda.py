4924b33f5d9236f52b8ee85a85ea01efc4956de2
Yusuf Mayet <yusmayet@amazon.com>
2022-04-26 15:15:08 +0200

    updated regex to not catch pipe character

--- TelegramTasweerBot/TelegramTasweerBot_Lambda.py
+++ TelegramTasweerBot/TelegramTasweerBot_Lambda.py
@@ -17,7 +17,7 @@ bot = Bot(token=TelegramBotToken)
 dispatcher = Dispatcher(bot, None, use_context=True)
 
 def lambda_handler(event, context):
-    emoji_blocklist = "[\U0001F600-\U0001F64B|\U0001F610-\U0001F61F|\U0001F937|\U0001F483|\U0001F435|\U0001F412|\U0001F920-\U0001F92F|\U0001F910-\U0001F917]" #Catches most of the F600 faces-range (except the last few which is the hands emojis). F937 is person shrugging, 1F483 is woman dancing, 1F435 and 1F412 are monkey. Catches some parts of the 1F900 range
+    emoji_blocklist = "([\U0001F600-\U0001F64B])|([\U0001F610-\U0001F61F])|([\U0001F937])|([\U0001F483])|([\U0001F435])|([\U0001F412])|([\U0001F920-\U0001F92F])|([\U0001F910-\U0001F917])" #Catches most of the F600 faces-range (except the last few which is the hands emojis). F937 is person shrugging, 1F483 is woman dancing, 1F435 and 1F412 are monkey. Catches some parts of the 1F900 range
     
     dispatcher.add_handler(MessageHandler(Filters.photo | Filters.document.image | Filters.document.jpg, image)) #to catch inline photos, and photos as attachements/files
     dispatcher.add_handler(MessageHandler(Filters.video | Filters.document.mime_type("video/mp4"), vid)) #to catch inline vidoes, and videos as attachements/files
