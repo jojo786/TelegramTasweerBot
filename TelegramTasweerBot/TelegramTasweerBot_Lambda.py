818e9fbd6e87927fe5d16fe9617c5cace5786eb5
Yusuf Mayet <jojo786@gmail.com>
2023-03-27 16:21:43 +0200

    Added more ranges to emoji regenx

--- TelegramTasweerBot/TelegramTasweerBot_Lambda.py
+++ TelegramTasweerBot/TelegramTasweerBot_Lambda.py
@@ -17,8 +17,30 @@ bot = Bot(token=TelegramBotToken)
 dispatcher = Dispatcher(bot, None, use_context=True)
 
 def lambda_handler(event, context):
-    emoji_blocklist = "([\U0001F600-\U0001F64B])|([\U0001F610-\U0001F61F])|([\U0001F937])|([\U0001F483])|([\U0001F435])|([\U0001F412])|([\U0001F920-\U0001F92F])|([\U0001F910-\U0001F917])" #Catches most of the F600 faces-range (except the last few which is the hands emojis). F937 is person shrugging, 1F483 is woman dancing, 1F435 and 1F412 are monkey. Catches some parts of the 1F900 range
-    
+    emoji_blocklist = "([\U0001F600-\U0001F64B])" #F600-F64B: Emoticons and pictographs, including various facial expressions - https://unicode.org/charts/PDF/U1F600.pdf
+    emoji_blocklist += "|([\U0001F910-\U0001F96B])" #F910-F96B: Additional emoticons and pictographs, including various facial expressions, hand gestures, and objects - https://unicode.org/charts/PDF/U1F900.pdf
+    emoji_blocklist += "|([\U0001F980-\U0001F991])" #F980-F991: Animal emojies - https://unicode.org/charts/PDF/U1F900.pdf
+    emoji_blocklist += "|[\U0001FAE0-\U0001FAE8])" #FAE0-FAE8: faces 
+    emoji_blocklist += "|([\U0001F937])|([\U0001F483])|([\U0001F435])|([\U0001F412])"  #F937 is person shrugging, F483 is woman dancing, F435 and F412 are monkeys, 
+    emoji_blocklist += "|([\U0001F466-\U0001F478])" # Various person emojis, such as man, woman, boy, girl, princess, baby                  
+    emoji_blocklist += "|([\U0001F481-\U0001F483])" # Dancer emojis, including woman dancing, man dancing, and people with bunny ears.
+    emoji_blocklist += "|([\U0001F486-\U0001F487])" # Various person emojis, such as woman getting haircut, man getting haircut, and person getting massage.
+
+#still to test
+#\U0001F645-\U0001F647: Various gesture emojis, such as person gesturing OK, person shrugging, and person tipping hand.
+#\U0001F64B-\U0001F64F: Various person emojis, such as person raising hand, person bowing, and person with folded hands.
+#\U0001F6A3: Rowboat emoji, which depicts a person rowing a boat.
+#\U0001F6B4-\U0001F6B6: Various person emojis, such as person biking, person mountain biking, and person walking.
+#\U0001F6C0: Bath emoji, which depicts a person in a bathtub.
+#\U0001F938: Prince emoji.
+#\U0001F93C: Person in lotus position emoji, which depicts a person sitting cross-legged with their eyes closed.
+#\U0001F93D: Skateboarder emoji.
+#\U0001F93E: Person surfing emoji.
+#\U0001F9D1-\U0001F9D3: Various person emojis, such as person in wheelchair, person with probing cane, and person with white cane.
+
+
+
+
     dispatcher.add_handler(MessageHandler(Filters.photo | Filters.document.image | Filters.document.jpg, image)) #to catch inline photos, and photos as attachements/files
     dispatcher.add_handler(MessageHandler(Filters.video | Filters.document.mime_type("video/mp4"), vid)) #to catch inline vidoes, and videos as attachements/files
     dispatcher.add_handler(CommandHandler("health", health))
